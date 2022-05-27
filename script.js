f6cef523e5da4e80fb2d0d256f411feb82e6b47a
silviawolves <silviacanepa.w@gmail.com>
2022-05-27 13:03:24 +0200

    codice base + consegna

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+
+
+/*
+Generare una griglia di gioco quadrata , in cui ogni cella contiene un numero incrementale tra quelli compresi tra 1 e 100
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+*/
